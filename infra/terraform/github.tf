e56e60817e13057c5e68276908092d1661365b39
Aleks <etc@idk.fm>
2024-03-19 23:36:39 +1300

    chore: added homepage url to github repo

--- infra/terraform/github.tf
+++ infra/terraform/github.tf
@@ -1,7 +1,8 @@
 resource "github_repository" "repo" {
-  name        = "idkfm"
-  description = "Monorepo for my portfolio website."
-  visibility  = "public"
+  name         = "idkfm"
+  description  = "Monorepo for my portfolio website."
+  visibility   = "public"
+  homepage_url = "https://idk.fm"
 
   has_downloads = false
   has_issues    = false
